bef185d4d2bead7ced3e51d23041b418d83a1067
hayz999 <hlzulkoski@gmail.com>
2018-05-25 11:55:45 -0600

    updated seed

--- seeds/00_dogs.js
+++ seeds/00_dogs.js
@@ -8,28 +8,28 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           dogName: 'Teddy',
-          imgURL: 'https://files.slack.com/files-pri/T1T555TL0-FAVNMFUQ0/20180515_100951.jpg',
+          imgURL: 'https://flic.kr/p/KoeTFm',
           rating: '14',
           comment: 'Found this sweet baby boy having a heckin\' good nap. Sweet dreams little prince.'
         },
         {
           id: 2,
           dogName: 'Klaus',
-          imgURL: 'https://files.slack.com/files-pri/T1T555TL0-FAVSYRF6V/image_uploaded_from_ios.jpg',
+          imgURL: 'https://flic.kr/p/KoeTqw',
           rating: '12',
           comment: 'Met this sweet baby yoga boy doin\' a heckin\' good stretch'
         },
         {
           id: 3,
           dogName: 'Chloe',
-          imgURL: 'https://files.slack.com/files-pri/T1T555TL0-FAW6YEBNG/image_uploaded_from_ios.jpg',
+          imgURL: 'https://flic.kr/p/24Luqbs',
           rating: '13',
           comment: 'Beautiful festive girl, got the memo about Aloha Fridays. Heckin\' good job!'
         },
         {
           id: 4,
           dogName: 'Quinn',
-          imgURL: 'https://files.slack.com/files-pri/T1T555TL0-FAW6YEBNG/image_uploaded_from_ios.jpg',
+          imgURL: 'https://flic.kr/p/24LupFu',
           rating: '14',
           comment: ''
         }
